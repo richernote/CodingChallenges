05625eb4ec993927b71735235ad3beddbede5a5e
richernote <richernote@gmail.com>
2022-03-17 07:10:50 -0700

    Finished revbits coding challenge. Commented thought process

--- revbits.js
+++ revbits.js
@@ -1,14 +1,18 @@
+// Codewars 'Data Reverse'
+// A stream of data is received and needs to be reversed.
+// Each segment is 8 bits long, meaning the order of these segments needs to be reversed
+// The total number of bits will always be a multiple of 8.
+
 function dataReverse(data) {
+    // create new array
     let box = []
-    let i = 0;
-    let newData = data
-    while (i < newData.length) {
-     box.push(data.slice(0, 8))
-     newData = newData.slice(8)
-      i++
+    // remove 8 elements at a time and add them to box
+    while (data.length > 0) {
+        box.unshift(data.slice(0, 8))
+        data = data.slice(8)
     }
-    
-    console.log(box)
+    // flatted nested array into one
+    return box.flatMap(n => n);
   }
 
 
