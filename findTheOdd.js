faf9779925594c6e21fa061c7e3e0dc705275e30
root <root@DESKTOP-5TD4F4I.localdomain>
2022-04-15 18:09:35 -0700

    forgot to put comments on the code

--- findTheOdd.js
+++ findTheOdd.js
@@ -3,11 +3,14 @@
 //There will always be only one integer that appears an odd number of times.
 
 function findOdd(A) {
+    // create new obj with "number": count
     let b = A.reduce((obj, name) =>{
       obj[name] = obj[name]? obj[name] += 1 : obj[name] = 1;
+      //return the object
       return obj}
                      ,{})
     
+    // find the one with the odd count
     for (let odd in b) {
       if (b[odd] % 2 != 0){
         return Number(odd)
