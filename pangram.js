4fe7bfdadd4ba9398dc322dad2cedebead17a50d
richernote <richernote@gmail.com>
2022-07-14 21:36:45 -0700

    forgot to add description of challenge

--- pangram.js
+++ pangram.js
@@ -1,3 +1,6 @@
+// A pangram is a sentence that contains every single letter of the alphabet at least once.
+// Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
 function isPangram(string){
     const alphaIndex = Array.from(Array(26)).map((e, i) => i + 65);
     const alphabet = alphaIndex.map(e => String.fromCharCode(e))
